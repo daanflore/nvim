1158d5f49bcd7fd80eba539ac3113183fa661129
daanflore <daanflore@gmail.com>
2025-06-14 15:39:30 +0200

    Adding inline error support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,2 +1,37 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- https://neovim.io/doc/user/autocmd.html --
+-- Autocommands are used to trigger actions based on events in Neovim.
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup('lsp-attach', {
+        clear = true
+    }),
+    callback = function(event)
+        vim.diagnostic.config({
+            virtual_lines = {
+                current_line = true
+            },
+            virtual_text = false,
+            underline = true,
+            update_in_insert = false,
+            severity_sort = true,
+            float = {
+                header = "",
+                border = "rounded",
+                source = true,
+                focusable = true
+            },
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = " ",
+                    [vim.diagnostic.severity.WARN] = "1 ",
+                    [vim.diagnostic.severity.INFO] = "2 ",
+                    [vim.diagnostic.severity.HINT] = "3 "
+                }
+            }
+        })
+    end
+})
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,12 @@
 return {
-  "mason-org/mason.nvim",
-  config = true
+    {
+        "mason-org/mason.nvim",
+        config = true
+    },
+	{
+		"mason-org/mason-lspconfig.nvim",
+        event = { "BufReadPost", "BufNewFile" },
+		dependencies = { "neovim/nvim-lspconfig" },
+		opts = {},
+	}
 }
