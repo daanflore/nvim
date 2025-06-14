89d95b1f63c574b14e27ee197dc434fb4b04924b
daanflore <daanflore@gmail.com>
2025-06-14 22:38:57 +0200

    add icons and colorin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -27,12 +27,28 @@ autocmd("LspAttach", {
             },
             signs = {
                 text = {
-                    [vim.diagnostic.severity.ERROR] = " ",
-                    [vim.diagnostic.severity.WARN] = "1 ",
-                    [vim.diagnostic.severity.INFO] = "2 ",
-                    [vim.diagnostic.severity.HINT] = "3 "
+                    [vim.diagnostic.severity.ERROR] = "",
+                    [vim.diagnostic.severity.WARN] = "",
+                    [vim.diagnostic.severity.INFO] = "",
+                    [vim.diagnostic.severity.HINT] = ""
                 }
             }
         })
+
+	-- Set coloring
+	vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = "#ff5555" })
+	vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn",  { undercurl = true, sp = "#f1fa8c" })
+	vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo",  { undercurl = true, sp = "#8be9fd" })
+	vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint",  { undercurl = true, sp = "#50fa7b" })
+
+	vim.api.nvim_set_hl(0, "DiagnosticSignError",      { fg = "#ff5555", bg = "NONE" })
+	vim.api.nvim_set_hl(0, "DiagnosticSignWarn",       { fg = "#f1fa8c", bg = "NONE" })
+	vim.api.nvim_set_hl(0, "DiagnosticSignInfo",       { fg = "#8be9fd", bg = "NONE" })
+	vim.api.nvim_set_hl(0, "DiagnosticSignHint",       { fg = "#50fa7b", bg = "NONE" })
+
+	vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#ff5555" })
+	vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn",  { fg = "#f1fa8c" })
+	vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo",  { fg = "#8be9fd" })
+	vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint",  { fg = "#50fa7b" })
     end
 })
